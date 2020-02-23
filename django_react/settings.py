8dc5980af31518b242df057ff876ffe0c078443c
Alex Yang <alex19911118@gmail.com>
2020-02-23 20:21:33 +0000

    :hammer: conf: using whitenoise for staticfiles

--- django_react/settings.py
+++ django_react/settings.py
@@ -126,8 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 from .settings_local import *
