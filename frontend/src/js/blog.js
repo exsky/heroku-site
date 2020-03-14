f96510dc3012513c112027bc755d73f1332d3a02
Alex Yang <alex19911118@gmail.com>
2020-03-14 03:00:20 +0000

    :art: fix nav active status

--- frontend/src/js/blog.js
+++ frontend/src/js/blog.js
@@ -1,23 +1,6 @@
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    $("#sidebar").css("width", "18%");
-    $("#sidebar").css("display", "block");
-    $("#content").css("margin-left", "22%");
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    $("#sidebar").css("width", "0");
-    $("#sidebar").css("display", "none");
-    $("#content").css("margin-left", "4%");
-}
-
 // A $( document ).ready() block.
 $( document ).ready(function() {
-    $("#sidebar-icon").click(function (){
-        openNav();
-    });
-    $("#close-sidebar-btn").click(function (){
-        closeNav();
-    });
+    /* Change active nav item to blog */
+    $(".nav").find(".active").removeClass("active");
+    $("#nav-blog").addClass("active");
 });
