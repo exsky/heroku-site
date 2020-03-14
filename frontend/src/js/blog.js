ff4b14005d4251646611c689012477d8f31de66d
Alex Yang <alex19911118@gmail.com>
2020-03-14 03:23:06 +0000

    :hammer: refactor templates

--- frontend/src/js/blog.js
+++ frontend/src/js/blog.js
@@ -1,6 +1,6 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
     /* Change active nav item to blog */
-    $(".nav").find(".active").removeClass("active");
     $("#nav-blog").addClass("active");
+    $(".nav").find(".active").not("#nav-blog").removeClass("active");
 });
